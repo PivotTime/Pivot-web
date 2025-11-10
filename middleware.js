51148c39469f9393d640205d4cad70bc1b1739ea
yeonsu0224 <yunsu0224@gmail.com>
2025-11-10 15:46:34 +0900

    add CountDown Page5

--- middleware.js
+++ middleware.js
@@ -10,7 +10,7 @@ export function middleware(req) {
 
 
   if (now.getTime() < target) {
-    url.pathname = "/CountDown"; 
+    url.pathname = "/countDown"; 
     return NextResponse.redirect(url);
   }
 
